f5a76c65ac33b6d2d89908ebe310be7bb94f533b
zeroistfilm <zeroistfilm@naver.com>
2021-01-10 16:46:02 +0900

    삽입, 검색 성공

--- bplus/btree.c
+++ bplus/btree.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-const int MinDeg= 3;
+#define MinDeg 3
 struct Node {
-    struct Node *C[2*MinDeg];
+    struct Node* C[2 * MinDeg+1];
     bool isLeaf;
-    int key[2*MinDeg-1];
+    int key[2 * MinDeg];
     int lenKey;
     int t; // 트리의 차수 정보
 };
 struct BTREE {
     int t;
-    struct Node *root;
+    struct Node* root;
 };
-struct Node *BtreeNodeCreate() {
-    struct Node *newNode = malloc(sizeof(struct Node));
+struct Node* BtreeNodeCreate() {
+    struct Node* newNode = malloc(sizeof(struct Node));
     newNode->isLeaf = true;
     newNode->lenKey = 0;
     return newNode;
 }
-void BtreeSearch(struct Node *node, int searchValue, int level) {
-    int i = 0;
+void BtreeSearch(struct Node* node, int searchValue, int level) {
+    int i = 1;
     while (i <= node->lenKey && searchValue > node->key[i]) {
         i += 1;
     }
     if (i <= node->lenKey && searchValue == node->key[i]) {
-        printf("%d found in %d level\n", node->key[i],level);
+        printf("found %d in %d\n", node->key[i], level);
         return;
-    } else if (node->isLeaf == true && searchValue == node->key[i]) {
-        printf("NULL in %d\n",level);
+    }
+    else if (node->isLeaf == true && searchValue == node->key[i]) {
+        printf("NULL in %d\n", level);
         return;
-    } else {
+    }
+    else {
         //printf("search child node\n");
-        BtreeSearch(node->C[i], searchValue,level+1);
+        BtreeSearch(node->C[i], searchValue, level + 1); //궁금,,
     }
 }
-void BtreeSplitChild(struct Node *ParentNode, int idx) {
-    struct Node *rightNode = BtreeNodeCreate();
-    struct Node *leftNode = ParentNode->C[idx];
+void BtreeSplitChild(struct Node* ParentNode, int idx) {
+    struct Node* rightNode = BtreeNodeCreate();
+    struct Node* leftNode = ParentNode->C[idx];
     int T = MinDeg;
     rightNode->isLeaf = leftNode->isLeaf;
     rightNode->lenKey = T - 1;
@@ -60,21 +62,22 @@ void BtreeSplitChild(struct Node *ParentNode, int idx) {
     ParentNode->key[idx] = leftNode->key[T];
     ParentNode->lenKey += 1;
 }
-void BtreeInsertNonFull(struct Node *node, int inputValue) {
+void BtreeInsertNonFull(struct Node* node, int inputValue) {
     int i = node->lenKey;
-    if (node->isLeaf == true ) {
+    if (node->isLeaf == true) {
         while (i >= 1 && inputValue < node->key[i]) {
             node->key[i + 1] = node->key[i];
             i -= 1;
         }
-        node->key[i] = inputValue;
+        node->key[i + 1] = inputValue;
         node->lenKey += 1;
-    } else {
+    }
+    else {
         while (i >= 1 && inputValue < node->key[i]) {
             i -= 1;
         }
         i += 1;
-        if (node->C[i]->lenKey == 2 * (MinDeg)-1) {
+        if (node->C[i]->lenKey == 2 * MinDeg - 1) {
             BtreeSplitChild(node, i);
             if (inputValue > node->key[i]) {
                 i += 1;
@@ -83,35 +86,37 @@ void BtreeInsertNonFull(struct Node *node, int inputValue) {
         BtreeInsertNonFull(node->C[i], inputValue);
     }
 }
-void BtreeInsertNode(struct BTREE *btree, int inputValue) {
-    struct Node *r = BtreeNodeCreate();
+void BtreeInsertNode(struct BTREE* btree, int inputValue) {
+    struct Node* r = BtreeNodeCreate();
     r = btree->root;
-    if (r->lenKey == 2 * (MinDeg) - 1) {
-        struct Node *s = BtreeNodeCreate();
+    if (r->lenKey == 2 * (MinDeg)-1) {
+        struct Node* s = BtreeNodeCreate();
         btree->root = s;
         s->isLeaf = false;
         s->lenKey = 0;
         s->C[1] = r;
         BtreeSplitChild(s, 1);
         BtreeInsertNonFull(s, inputValue);
-    } else {
+    }
+    else {
         BtreeInsertNonFull(r, inputValue);
     }
 }
-void BtreeCreate(struct BTREE *T){
+void BtreeCreate(struct BTREE* T) {
     struct Node* x = BtreeNodeCreate();
     T->root = x;
 }
 int main() {
-    struct BTREE *BTree = malloc(sizeof(struct BTREE));
+    struct BTREE* BTree = malloc(sizeof(struct BTREE));
     BtreeCreate(BTree);
-    for(int i =1; i<12; i++)
+    for (int i = 1; i <= 2000; i++)
     {
-        BtreeInsertNode(BTree,i);
-        for(int j =1 ;j<i; j++)
-        {
-            BtreeSearch(BTree->root,j,0);
-        }
+        BtreeInsertNode(BTree, i);
+
         printf("==================\n");
+    }
+    for (int j = 1; j <= 2000; j++)
+    {
+        BtreeSearch(BTree->root, j, 0);
     }
 }
